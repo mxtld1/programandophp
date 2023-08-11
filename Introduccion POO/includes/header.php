07704b8484ab451fc504f2f2b2bc44a535080dea
Max Toledo <mx.tld1@gmail.com>
2023-08-11 01:02:07 -0600

    Header Actualizado

--- Introduccion POO/includes/header.php
+++ Introduccion POO/includes/header.php
@@ -4,17 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Programación Orientada a Objetos PHP  - Código Con Juan</title>
+    <title>Programación Orientada a Objetos PHP  - La base del Codigo</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css">
 </head>
 <body class="bg-gray-50">
     <div class="mt-10 max-w-2xl mx-auto text-center space-y-3">
         <h1 class="text-4xl font-extrabold text-indigo-600 mb-10" >
-            Programación Orientada a Objetos PHP  - Código Con Juan
+            Programación Orientada a Objetos PHP  - La base del Codigo
         </h1>
-        <a class="block" href="http://www.codigoconjuan.com" target="_blank">Sitio Web: codigoconjuan.com</a>
-        <a class="block" href="https://twitter.com/codigoconjuan" target="_blank">Twitter: @codigoconjuan</a>
-        <p class="text-2xl text-indigo-600 block">#codigoConJuan</p>
+        <a class="block" href="http://www.labasedelcodigo.com" target="_blank">Sitio Web: labasedelcodigo.com</a>
+        <a class="block" href="https://twitter.com/labasedelcodigo" target="_blank">Twitter: @labasedelcodigo</a>
+        <p class="text-2xl text-indigo-600 block">#labasedelcodigo</p>
     </div>
 
     <div class="mt-10 max-w-2xl mx-auto shadow bg-white p-10 text-base">
